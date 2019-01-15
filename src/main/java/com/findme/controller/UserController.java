4d26190800851b39d5539222ab76088dae0b1c5e
Zhenya Shepel <shepelzz@gmail.com>
2019-01-15 17:41:59 +0200

    adding profile edit form

--- src/main/java/com/findme/controller/UserController.java
+++ src/main/java/com/findme/controller/UserController.java
@@ -69,6 +69,57 @@ public class UserController {
         }
     }
 
+    @RequestMapping(path = "/edit-user/{userId}", method = RequestMethod.GET)
+    public String editUserInfo(HttpSession session, Model model, @PathVariable String userId){
+        String loggedUserId = (String) session.getAttribute("loggedUserId");
+        if(loggedUserId==null || !loggedUserId.equals(userId)) {
+            model.addAttribute("error", new BadRequestException("You are not logged in to see this information."));
+            return "errors/forbidden";
+        }
+        try {
+            model.addAttribute("user", userService.findById(Long.valueOf(userId)));
+            return "profileEdit";
+        } catch (NumberFormatException e){
+            model.addAttribute("error", e);
+            return "errors/badRequest";
+        } catch (InternalServerError ise){
+            model.addAttribute("error", ise);
+            return "errors/internalServerError";
+        } catch (NotFoundException nofe){
+            model.addAttribute("error", nofe);
+            return "errors/notFound";
+        }
+    }
+
+    @RequestMapping(path = "/edit-user", method = RequestMethod.POST)
+    public ResponseEntity<String> editUserSubmit(HttpSession session, @ModelAttribute User user){
+        if(session.getAttribute("loggedUserId")==null) {
+            return new ResponseEntity<>("You are not logged in to see this information.", HttpStatus.FORBIDDEN);
+        }
+        try {
+//            user.setId(Long.valueOf(userId));
+            userService.update(user);
+            return new ResponseEntity<>( HttpStatus.OK);
+        } catch (BadRequestException e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        } catch (InternalServerError | NumberFormatException e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @RequestMapping(path = "/remove-user/{userId}", method = RequestMethod.DELETE)
+    public ResponseEntity<String> deleteUser(HttpSession session, @PathVariable String userId){
+        if(session.getAttribute("loggedUserId")==null) {
+            return new ResponseEntity<>("You are not logged in to see this information.", HttpStatus.FORBIDDEN);
+        }
+        try {
+            userService.delete(Long.valueOf(userId));
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (InternalServerError e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
     @RequestMapping(path = "/register-user", method = RequestMethod.POST)
     public ResponseEntity<String> registerUser(@ModelAttribute User user){
         try {
