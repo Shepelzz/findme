995f1a037e17eacf7419c29e80d52af1c482f568
Zhenya Shepel <shepelzz@gmail.com>
2019-02-20 11:03:58 +0200

    test ajax worked

--- src/main/java/com/findme/test/RestWebController.java
+++ src/main/java/com/findme/test/RestWebController.java
@@ -1,8 +1,5 @@
 package com.findme.test;
 
-import com.findme.exception.BadRequestException;
-import com.findme.exception.InternalServerError;
-import com.findme.model.FilterPagePosts;
 import com.findme.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,52 +20,19 @@ public class RestWebController {
         this.postService = postService;
     }
 
-    //TODO
-    //https://www.mkyong.com/spring-boot/spring-boot-ajax-example/
-    private List<Customer> cust = new ArrayList<>();
-
-//    @GetMapping(value = "/all")
-//    public Response getResource() {
-//        return new Response("Done", cust);
-//    }
-
+    List<Customer> cust = new ArrayList<Customer>();
 
     @GetMapping(value = "/all")
-    public ResponseEntity<?> getResource() throws Exception{
-        try {
-            return new ResponseEntity<>(postService.getNewsList(1L, 5), HttpStatus.OK);
-        } catch (Exception e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
+    public ResponseEntity<?> getResource() {
+        return new ResponseEntity<>(cust, HttpStatus.OK);
     }
-
-//    @PostMapping(value = "/save")
-//    public Response postCustomer(@RequestBody Customer customer) {
-//        cust.add(customer);
-//
-//        // Create Response Object
-//        return new Response("Done", customer);
-//    }
 
     @PostMapping(value = "/save")
-    public ResponseEntity<?> postCustomer(@RequestBody Customer customer) {
+    public ResponseEntity<?> postCustomer(@ModelAttribute Customer customer) {
 
-        if(!customer.getFirstname().equals("gog")){
-            cust.add(customer);
-            return new ResponseEntity<>(customer, HttpStatus.OK);
-        }
-        return new ResponseEntity<>("err", HttpStatus.NOT_FOUND);
-    }
-
-    @PostMapping(value = "/posts-by-filter")
-    public ResponseEntity<?> posts(@RequestBody FilterPagePosts filter) {
-
-        try {
-            return new ResponseEntity<>(postService.getPostsByFilter("1", filter).toString(), HttpStatus.OK);
-        } catch (BadRequestException e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        } catch (InternalServerError e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        if(customer.getFirstname().equals("gog"))
+            return new ResponseEntity<>("not gog!", HttpStatus.BAD_REQUEST);
+        cust.add(customer);
+        return new ResponseEntity<>(customer, HttpStatus.OK);
     }
 }
