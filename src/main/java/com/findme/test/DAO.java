4c572cbe1cbec8acb951e01e079ac5db10569719
Zhenya Shepel <shepelzz@gmail.com>
2019-03-18 17:06:34 +0200

    messenger edit/delete

--- src/main/java/com/findme/test/DAO.java
+++ src/main/java/com/findme/test/DAO.java
@@ -1,11 +1,12 @@
 package com.findme.test;
 
 import com.findme.exception.InternalServerError;
-import com.findme.model.Relationship;
 import lombok.extern.log4j.Log4j;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.*;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.util.List;
 import java.util.Map;
 
@@ -22,57 +23,48 @@ public class DAO {
 
 
         String SQL_GET_RELATIONSHIP = "" +
-                "SELECT  new com.findme.test.ConversationInfo(2," +
-                "        'dfdf'," +
-                "        'eeee'," +
-                "        4," +
-                "        'dfdfd'," +
-                "        1," +
-                "        11," +
-                "        '2019-01-01'," +
-                "        '2019-02-02')" +
-                " FROM User u";
+                "SELECT  DATA.userId,\n" +
+                "    DATA.user_firstName,\n" +
+                "    DATA.user_lastName,\n" +
+                "    m.ID msg_id,\n" +
+                "    m.TEXT msg_text,\n" +
+                "    m.USER_FROM_ID msg_userFromId,\n" +
+                "    m.USER_TO_ID msg_userToId,\n" +
+                "    m.DATE_SENT msg_dateSent,\n" +
+                "    m.DATE_READ msg_dateRead\n" +
+                "    FROM (\n" +
+                "            SELECT\n" +
+                "                    u.id         userId,\n" +
+                "            u.LAST_NAME  user_lastName,\n" +
+                "            u.FIRST_NAME user_firstName,\n" +
+                "            MAX(msg.Id) max_msgId\n" +
+                "    FROM RELATIONSHIP r\n" +
+                "    JOIN USERS u\n" +
+                "    ON ((r.USER_FROM_ID = :userId AND r.USER_TO_ID = u.id) OR (r.USER_TO_ID = :userId AND r.USER_FROM_ID = u.id))\n" +
+                "    LEFT JOIN MESSAGE msg ON ((msg.USER_FROM_ID = u.id AND msg.USER_TO_ID = :userId) OR\n" +
+                "                              (msg.USER_FROM_ID = :userId AND msg.USER_TO_ID = u.id))\n" +
+                "    AND msg.DATE_DELETED IS NULL\n" +
+                "    WHERE r.STATUS = 'FRIENDS'\n" +
+                "    GROUP BY u.id,\n" +
+                "    u.LAST_NAME,\n" +
+                "    u.FIRST_NAME\n" +
+                ") DATA\n" +
+                "    LEFT JOIN MESSAGE m ON DATA.max_msgId = m.ID\n" +
+                "    ORDER BY m.DATE_SENT DESC, DATA.user_lastName ASC";
 
         try {
-            TypedQuery<ConversationInfo> query = entityManager.createQuery(SQL_GET_RELATIONSHIP, ConversationInfo.class);
-
-
-            return query.getResultList();
+            Query query = entityManager.createNativeQuery(SQL_GET_RELATIONSHIP);
+            query.setParameter("userId", userId);
+            List<Map> list = query.getResultList();
         }catch (Exception e){
             log.error(e.getMessage(), e);
             throw new InternalServerError(e.getMessage(), e.getCause());
         }
+        return null;
     }
 
 
-//    SELECT  DATA.userId,
-//    DATA.user_firstName,
-//    DATA.user_lastName,
-//    m.ID msg_id,
-//    m.TEXT msg_text,
-//    m.USER_FROM_ID msg_userFromId,
-//    m.USER_TO_ID msg_userToId,
-//    m.DATE_SENT msg_dateSent,
-//    m.DATE_READ msg_dateRead
-//    FROM (
-//            SELECT
-//                    u.id         userId,
-//            u.LAST_NAME  user_lastName,
-//            u.FIRST_NAME user_firstName,
-//            MAX(msg.Id) max_msgId
-//    FROM RELATIONSHIP r
-//    JOIN USERS u
-//    ON ((r.USER_FROM_ID = :userId AND r.USER_TO_ID = u.id) OR (r.USER_TO_ID = :userId AND r.USER_FROM_ID = u.id))
-//    LEFT JOIN MESSAGE msg ON ((msg.USER_FROM_ID = u.id AND msg.USER_TO_ID = :userId) OR
-//                              (msg.USER_FROM_ID = :userId AND msg.USER_TO_ID = u.id))
-//    AND msg.DATE_DELETED IS NULL
-//    WHERE r.STATUS = 'FRIENDS'
-//    GROUP BY u.id,
-//    u.LAST_NAME,
-//    u.FIRST_NAME
-//) DATA
-//    LEFT JOIN MESSAGE m ON DATA.max_msgId = m.ID
-//    ORDER BY m.DATE_SENT DESC, DATA.user_lastName ASC
+
 
 
 
