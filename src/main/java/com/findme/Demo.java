bad6545bbad97a0f6845305d8c5ff233069b8f30
shepelzz <shepelzz@gmail.com>
2019-07-29 16:07:40 +0300

    run

--- src/main/java/com/findme/Demo.java
+++ src/main/java/com/findme/Demo.java
@@ -1,9 +1,13 @@
 package com.findme;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Demo {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
-
+        SpringApplication.run(Demo.class, args);
     }
 
 
